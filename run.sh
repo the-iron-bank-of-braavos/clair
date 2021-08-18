218f6a260b7381498fbcacc7cd5c2d3f72561102
Iván Amat <iamat@atsistemas.com>
2021-08-18 09:18:16 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,20 @@
 
 docker-compose up -d
 
+timetorun=30
+stoptime=$((timetorun + $(date +%s)))
+
 # Check if Clair is UP!
-STOP=0
 while [ true ]
 do
-    echo "While number: $STOP"
-    if [[ "$STOP" -eq 19 ]]; then
-        echo "STOP!"
+    if [[ $(date +%s) > $stoptime ]]; then
         break;
     fi
+    
     curl http://localhost:6060 &> /dev/null 2>&1
-    if [[ "$?" -eq 0 ]]; then
-	echo "Clair is ready!"
+    
+    if [ "$?" -eq 0 ]; then
+        echo "Clair is ready!"
         exit 0
     fi
-    STOP=$STOP+1
 done
