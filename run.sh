58f03002c01cbc4166aa43495b151de4d673147b
Iván Amat <iamat@atsistemas.com>
2021-08-18 08:53:51 +0200

    TEST STOP

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 docker-compose up -d
 
 # Check if Clair is UP!
+STOP=0
 while [ true ]
 do
-    curl http://localhost:6060 &> /dev/null
+    if [[ "$STOP" -eq 4 ]]; then
+        echo "STOP!"
+        break;
+    fi
+    curl http://localhost:6060 &> /dev/null 2>&1
     if [[ "$?" -eq 0 ]]; then
 	echo "Clair is ready!"
         exit 0
     fi
+    STOP=$STOP+1
 done
