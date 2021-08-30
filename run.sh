87ea9d832987ede67fe883f8435681b6935537ed
Iván Amat <iamat@atsistemas.com>
2021-08-30 13:59:52 +0200

    Updated run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,19 @@ docker-compose -f $GITHUB_ACTION_PATH/docker-compose.yml up -d registry postgres
 timetorun=30
 stoptime=$((timetorun + $(date +%s)))
 
+function checkClairStatus() {
+    DONE=$(docker logs -n 1000 clair 2>&1 | jq 'select(.message=="starting background updates")' -)
+
+    if [ -z "$DONE" ]; then
+	return 0;
+    else
+        return 1;
+    fi
+}
+
 echo "Checking postgres status..."
 while [ true ]
-do  
+do 
     if [[ $(date +%s) > $stoptime ]]; then
         echo "[Error]: Timeout waiting for Postgres"
         docker logs -n 1000 postgres
@@ -15,7 +25,7 @@ do
     
     docker exec postgres psql -U clair -d clair &> /dev/null 2>&1
     
-    if [ "$?" -eq 0 ]; then
+    if [[ "$?" -eq 0 ]]; then
         echo "Postgres is ready!"
         docker-compose -f $GITHUB_ACTION_PATH/docker-compose.yml up -d clair
         break;
@@ -35,10 +45,13 @@ do
         docker logs -n 1000 clair
         exit 1;
     fi
+
+    checkClairStatus
+    STATUS=$?
     
     curl --max-time 5 http://localhost:6060 &> /dev/null 2>&1
     
-    if [ "$?" -eq 0 ]; then
+    if [[ "$?" -eq 0 && $STATUS -eq 1 ]]; then
         echo "Clair is ready!"
         break;
     fi
